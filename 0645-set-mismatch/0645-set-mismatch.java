51e6ff3e0bf9d50c3d18e4c3e2d67da8b5fac409
Sagar-Singh-Raghav <105165285+Sagar20-12@users.noreply.github.com>
2024-01-22 18:59:01 +0530

    Time: 19 ms (6.86%), Space: 45.9 MB (11.88%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,7 +1,5 @@
 //For brute force approach we can use Binary search in this.
-import java.util.Arrays;
-
-class Solution {
+/* class Solution {
     public int[] findErrorNums(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
@@ -38,5 +36,32 @@ class Solution {
                 high = mid - 1;
         }
         return false;
+    }
+}
+*/
+
+
+// Optimal Approach using HashMap
+
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] arr = {1,1}; 
+        int n = nums.length;
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i< n ;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        
+        for(int i = 1; i< n+1 ;i++){
+            if(map.containsKey(i)){
+                if (map.get(i) == 2){
+                    arr[0] = i;
+                }
+            }
+            else{ 
+                arr[1] = i;
+            }
+        }
+        return arr;
     }
 }
