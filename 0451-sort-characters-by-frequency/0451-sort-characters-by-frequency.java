dc15a747542378a75490854806fe9eb27e123b6c
Sagar-Singh-Raghav <105165285+Sagar20-12@users.noreply.github.com>
2024-02-07 18:12:04 +0530

    Time: 11 ms (86.45%), Space: 45.5 MB (48.36%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,4 +1,4 @@
-class Solution {
+/* class Solution {
     public String frequencySort(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
         for(char c : s.toCharArray())
@@ -10,6 +10,38 @@ class Solution {
             int count = map.get(c);
             for(int i = 0; i<count; i++){
                 res.append(c);
+            }
+        }
+        return res.toString();
+    }
+}
+*/
+class Solution {
+    public String frequencySort(String s) {
+
+        // Here we have created HashMap to store the frequency of the elements.
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c : s.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+        // Function to sort the character based on their frequency
+        TreeMap<Integer, List<Character>> sortedMap = new TreeMap<>(Collections.reverseOrder());
+        for (char key : map.keySet()) {
+            int freq = map.get(key);
+            if (!sortedMap.containsKey(freq)) {
+                sortedMap.put(freq, new ArrayList<>());
+            }
+            sortedMap.get(freq).add(key);
+        }
+ 
+        StringBuilder res = new StringBuilder();
+        for (int freq : sortedMap.keySet()) {
+            List<Character> characters = sortedMap.get(freq);
+            for (char c : characters) {
+                int count = map.get(c);
+                for (int i = 0; i < count; i++) {
+                    res.append(c);
+                }
             }
         }
         return res.toString();
