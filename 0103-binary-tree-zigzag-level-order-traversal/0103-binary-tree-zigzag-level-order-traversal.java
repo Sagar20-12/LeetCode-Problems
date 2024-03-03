ac5fcc9b376009ee0848cccb9b6bcabf5bff3067
Sagar-Singh-Raghav <105165285+Sagar20-12@users.noreply.github.com>
2024-03-03 23:31:58 +0530

    Time: 1 ms (76.89%), Space: 41.9 MB (77.03%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,32 +15,37 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> list = new ArrayList<>();
-        if(root == null){
-            return list;
-        }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        int level = 0;
-        while(!queue.isEmpty()){
-            int len = queue.size();
-            List<Integer>levellist = new ArrayList<>();
-            for(int i = 0; i<len; i++){
-                TreeNode temp = queue.poll();
-                if(temp.left!=null)
-                    queue.add(temp.left);
-                if(temp.right!=null)
-                    queue.add(temp.right);
+        List<List<Integer>> wrapList = new ArrayList<>();
+        if (root == null) return wrapList;
 
-                if(level%2 == 0)
-                    levellist.add(temp.val);
-                else
-                    levellist.add(0, temp.val);
-            }
-            level++;
-            list.add(levellist);
-        }
-        return list;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean flag = true;
+        //0 means left to right
+        //1 means right to left
         
+
+        while (!queue.isEmpty()) {
+            int levelNum = queue.size();
+            List<Integer> subList = new ArrayList<>(levelNum);
+
+            for (int i = 0; i < levelNum; i++) {
+                TreeNode node = queue.poll();
+
+                if (flag) {
+                    subList.add(node.val);
+                } else {
+                    subList.add(0, node.val);
+                }
+
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+
+            flag = !flag;
+            wrapList.add(subList);
+        }
+
+        return wrapList;
     }
 }
