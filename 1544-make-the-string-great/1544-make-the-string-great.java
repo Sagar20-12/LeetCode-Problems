2568a9e3d8174e272c27a8514343dd76db7f8ac8
Sagar-Singh-Raghav <105165285+Sagar20-12@users.noreply.github.com>
2024-04-05 18:06:02 +0530

    Time: 3 ms (61.07%), Space: 42 MB (77.06%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,16 +1,35 @@
+// class Solution {
+//     public String makeGood(String s) {
+//         StringBuilder sb = new StringBuilder();
+//         for(char c : s.toCharArray()){
+//             int size = sb.length();
+//             //if there is any character present and
+//             //their difference is 32(bcoz ascii values)
+//             if(size>0 && Math.abs(sb.charAt(size-1)-c) == 32){
+//                 sb.deleteCharAt(size-1);
+//             } else{
+//                 sb.append(c);
+//             }
+//         }
+//         return sb.toString();
+//     }
+// }
+
+
 class Solution {
-    public String makeGood(String s) {
-        StringBuilder sb = new StringBuilder();
-        for(char c : s.toCharArray()){
-            int size = sb.length();
-            //if there is any character present and
-            //their difference is 32(bcoz ascii values)
-            if(size>0 && Math.abs(sb.charAt(size-1)-c) == 32){
-                sb.deleteCharAt(size-1);
-            } else{
-                sb.append(c);
+     public String makeGood(String s) {
+         Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (!st.isEmpty() && Math.abs(st.peek() - c) == 32) {
+                st.pop();
+            } else {
+                st.push(c);
             }
         }
-        return sb.toString();
-    }
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty()) {
+            sb.append(st.pop());
+        }
+        return sb.reverse().toString();
+     }
 }
