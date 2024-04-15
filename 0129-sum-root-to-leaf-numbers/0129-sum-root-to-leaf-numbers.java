8bf39c94dab852abd65eec299e26db2d6c21460a
Sagar-Singh-Raghav <105165285+Sagar20-12@users.noreply.github.com>
2024-04-15 11:57:35 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (86.84%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -14,19 +14,23 @@
  * }
  */
 class Solution {
-    int result=0;
-    public void sum(TreeNode root, int num) {
-        if(root == null) return;
-        num = root.val + ( 10 * num);
-
-        if(root.left == null && root.right == null){
-            result += num;
-        }
-        sum(root.left, num);
-        sum(root.right, num);
-    }
+    //This is global variable, we havent taken inside the main function because it 
+    // will make this value to 0 zero only after iteration
+    int sum = 0;
     public int sumNumbers(TreeNode root) {
-        sum(root, 0);
-        return result;
+        getsum(root, 0);
+        return sum;
+    }
+    public void getsum(TreeNode root, int num){
+        if(root == null){
+            return;
+        }
+        num = num * 10 + root.val;
+        if(root.left == null && root.right == null){
+            sum += num;
+            return;
+        }
+        getsum(root.left, num);
+        getsum(root.right, num);
     }
 }
