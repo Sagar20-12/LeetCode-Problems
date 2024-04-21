62a88c49a2a848caa1c77fc07a4b54ee611a0278
Sagar-Singh-Raghav <105165285+Sagar20-12@users.noreply.github.com>
2024-04-21 20:43:54 +0530

    Time: 2 ms (100.00%), Space: 108.8 MB (97.02%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        // If there is only one edge
+       if(edges.length == 0)
+        return true;
+        boolean[] vis = new boolean[n];
+        vis[source] = true;
+        boolean flag = true;
+        while(flag){
+            flag = false;
+            for(int[] edge : edges){
+                if(vis[edge[0]]!=vis[edge[1]]){
+                    vis[edge[0]] = true;
+                    vis[edge[1]] = true;
+                    flag = true;
+                }
+                if(vis[destination]) return true;
+            }
+        }
+        return false;
+
+    }
+}
